527a5c8f3d7fc426192dd5b520f6b57bc590eafc
jomitigs <jomitigs@gmail.com>
2024-12-30 15:54:01 -0500

    Actualizacion automatica

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,20 @@ import postcss from 'rollup-plugin-postcss';
 import terser from '@rollup/plugin-terser';
 
 export default {
-  input: 'src/barralateral/script.js', // Archivo de entrada principal
+  input: 'src/barralateral/script.js',
   output: {
-    file: 'dist/autoquizfill.bundle.js', // Archivo de salida
-    format: 'iife', // Formato inmediatamente ejecutable
-    name: 'AutoQuizFillApp', // Nombre del módulo global
+    file: 'dist/autoquizfill.bundle.js',
+    format: 'iife',
+    name: 'AutoQuizFillApp',
   },
   plugins: [
     html({
-      include: 'src/barralateral/*.html', // Incluye archivos HTML
+      include: '**/*.html', // Incluye todos los archivos HTML
     }),
     postcss({
       extensions: ['.css'], // Procesa archivos CSS
       inject: true, // Inserta CSS en línea en el archivo final
     }),
-    // terser(), // Minifica el archivo final
+    terser(), // Minifica el archivo final
   ],
 };
