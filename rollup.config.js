1898bb6202ac9e843879f4299bea3d5d6b7dda7a
jomitigs <jomitigs@gmail.com>
2024-12-30 13:49:52 -0500

    Actualizaci├│n autom├ítica del script

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import terser from '@rollup/plugin-terser';
 export default {
   input: 'src/barralateral.js',
   output: {
-    file: 'dist/hello-world.bundle.js',
+    file: 'dist/barralateral.js',
     format: 'iife', // Formato inmediatamente ejecutable
     name: 'AutoQuizFillApp', // Nombre del módulo global
   },
