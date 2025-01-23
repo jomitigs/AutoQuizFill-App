695eea66d384f9aaf27df15a9d2fc47436cb81e7
jomitigs <116705031+jomitigs@users.noreply.github.com>
2025-01-22 22:06:04 -0500

    Corrige configuración y optimiza el código

--- rollup.config.js
+++ rollup.config.js
@@ -6,65 +6,26 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { string } from 'rollup-plugin-string';
 
-/**
- * Función para generar configuraciones de Rollup
- * @param {Object} options - Opciones específicas para cada build
- * @param {string} options.input - Archivo de entrada
- * @param {string} options.file - Archivo de salida
- * @param {string} options.name - Nombre global (opcional)
- * @param {Array<string>} [options.external] - Dependencias externas (opcional)
- * @param {Object} [options.globals] - Nombres globales para dependencias externas (opcional)
- * @returns {Object} Configuración de Rollup
- */
-function createConfig({ input, file, name, external = [], globals = {} }) {
-  return {
-    input,
-    output: {
-      file,
-      format: 'iife',
-      name,
-      globals,
-    },
-    external, // Si deseas marcar otras dependencias como externas
-    plugins: [
-      resolve(),
-      commonjs(),
-      string({
-        include: '**/*.html', // Permite importar archivos HTML como cadenas de texto
-      }),
-      postcss({
-        extensions: ['.css'],
-        inject: true,
-      }),
-      terser(),
-      html({
-        include: '**/*.html',
-      }),
-    ],
-    
-  };
-}
-
-export default [
-  // Configuración para agregar head
-  createConfig({
-    input: 'src/add-head/script.js',
-    file: 'dist/add-head.bundle.js',
-    name: 'AddHeadBundle',
-  }),
-  // Configuración para la barra lateral
-  createConfig({
-    input: 'src/barra-lateral/script.js',
-    file: 'dist/barra-lateral.bundle.js',
-    name: 'BarraLateralBundle',
-  }),
-  // Configuración para Firebase (Incluido en el bundle)
-  createConfig({
-    input: 'src/config-firebase/script.js',
-    file: 'dist/config-firebase.bundle.js',
-    name: 'FirebaseConfig',
-    // No marcar Firebase como externo para incluirlo en el bundle
-    // external: ['firebase/app', 'firebase/auth', 'firebase/database'], // ELIMINAR
-    // globals: { ... }, // ELIMINAR
-  }),
-];
+export default {
+  input: 'src/main-app.js',            // Único punto de entrada
+  output: {
+    file: 'dist/main-app.bundle.js',        // Un solo archivo de salida
+    format: 'iife',
+    name: 'MyBundle',              // Nombre global (opcional)
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    string({
+      include: '**/*.html', // Si necesitas importar archivos HTML como strings
+    }),
+    postcss({
+      extensions: ['.css'],
+      inject: true,
+    }),
+    terser(),
+    html({
+      include: '**/*.html',
+    }),
+  ],
+};
---
+++ src/main-app.js
@@ -0,0 +1,4 @@
+// src/main.js
+import './add-head/script.js';
+import './barra-lateral/script.js';
+import './config-firebase/script.js';
