7638a56c2291edfde659b1312e2aa60b58cb251c
jomitigs <116705031+jomitigs@users.noreply.github.com>
2025-01-22 22:32:27 -0500

    AutoGuardado

--- src/config-firebase/script.js
+++ src/config-firebase/script.js
@@ -9,21 +9,28 @@ import { getDatabase } from 'firebase/database';
 // Configuración de Firebase
 // =====================
 const configuracionFirebaseAutoQuiz = {
-    apiKey: "AIzaSyANIA0nk7P3RTo33P86jmW3GM6jMxxdoAs",
-    authDomain: "moodlequizdatascraping.firebaseapp.com",
-    databaseURL: "https://moodlequizdatascraping-default-rtdb.firebaseio.com",
-    projectId: "moodlequizdatascraping",
-    storageBucket: "moodlequizdatascraping.appspot.com",
-    messagingSenderId: "782692660220",
-    appId: "1:782692660220:web:8258d30da03e338f4c3879"
+  apiKey: "AIzaSyANIA0nk7P3RTo33P86jmW3GM6jMxxdoAs",
+  authDomain: "moodlequizdatascraping.firebaseapp.com",
+  databaseURL: "https://moodlequizdatascraping-default-rtdb.firebaseio.com",
+  projectId: "moodlequizdatascraping",
+  storageBucket: "moodlequizdatascraping.appspot.com",
+  messagingSenderId: "782692660220",
+  appId: "1:782692660220:web:8258d30da03e338f4c3879"
 };
+
+// Muestra la configuración en la consola para depuración
+console.log("Configuración de Firebase:", configuracionFirebaseAutoQuiz);
 
 // Inicializa Firebase
 const app = initializeApp(configuracionFirebaseAutoQuiz);
+console.log("Firebase App inicializada:", app);
 
 // Inicializa servicios de Firebase
 const autenticacion = getAuth(app);
+console.log("Servicio de autenticación (Auth) inicializado:", autenticacion);
+
 const database = getDatabase(app);
+console.log("Servicio de base de datos (Database) inicializado:", database);
 
 // Exponer las variables en el objeto global `window`
 window.autenticacion = autenticacion;
