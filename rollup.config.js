fb0856a7b16d8bbe231cb7f4959555421d73551c
jomitigs <116705031+jomitigs@users.noreply.github.com>
2025-01-26 20:56:28 -0500

    AutoGuardado

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,17 @@
-// rollup.config.js
 import html from '@rollup/plugin-html';
 import postcss from 'rollup-plugin-postcss';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { string } from 'rollup-plugin-string';
-
+import obfuscator from 'rollup-plugin-obfuscator';
 
 export default {
-  input: 'src/main-app.js',            // Único punto de entrada
+  input: 'src/main-app.js', // Único punto de entrada
   output: {
-    file: 'dist/main-app.bundle.js',        // Un solo archivo de salida
+    file: 'dist/main-app.bundle.js', // Un solo archivo de salida
     format: 'iife',
-    name: 'AutoQuizFillApp',              // Nombre global (opcional)
+    name: 'AutoQuizFillApp', // Nombre global (opcional)
   },
   plugins: [
     resolve({ browser: true }),
@@ -24,7 +23,26 @@ export default {
       extensions: ['.css'],
       inject: true,
     }),
-    // terser(),
+    terser(), // Minificación del código
+    obfuscator({ // Ofuscación del código
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.75,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 0.4,
+      debugProtection: false,
+      debugProtectionInterval: false,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: 'hexadecimal',
+      renameGlobals: false,
+      selfDefending: true,
+      splitStrings: true,
+      stringArray: true,
+      stringArrayEncoding: ['base64'], // Opcional: base64 o rc4
+      stringArrayThreshold: 0.75,
+      transformObjectKeys: true,
+      unicodeEscapeSequence: false,
+    }),
     html({
       include: '**/*.html',
     }),
