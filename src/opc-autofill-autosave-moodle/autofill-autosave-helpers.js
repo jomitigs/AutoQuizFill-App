4f5a178ee08efedc48805271eb898e0b9a1eb279
jomitigs <116705031+jomitigs@users.noreply.github.com>
2025-02-02 21:53:43 -0500

    AutoGuardado

--- src/opc-autofill-autosave-moodle/autofill-autosave-helpers.js
+++ src/opc-autofill-autosave-moodle/autofill-autosave-helpers.js
@@ -1,12 +1,4 @@
 
-// Importaciones de MathJax:
-import { mathjax } from 'mathjax-full/js/mathjax.js'
-import { TeX } from 'mathjax-full/js/input/tex.js'
-import { MathML } from 'mathjax-full/js/output/mathml.js'
-import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor.js'
-import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html.js'
-import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages.js'
-
 export async function feedbackQuestion(originalFormulationClearfix) {
     // console.log('Iniciando feedbackQuestion');
 
@@ -301,9 +293,6 @@ export async function File2DataUri(files) {
       }
     }
   }
-
-
-  
 
   export async function extractContentInOrder(node) {
     let content = '';
