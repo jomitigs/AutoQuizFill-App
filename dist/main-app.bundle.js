34a793d7304c5e6adf051f7bf7bd128ad4635754
jomitigs <116705031+jomitigs@users.noreply.github.com>
2025-02-13 00:40:05 -0500

    AutoGuardado

--- dist/main-app.bundle.js
+++ dist/main-app.bundle.js
@@ -45924,95 +45924,95 @@
 
 	function opcion_AutoFillAutoSave_Moodle_html() {
 	    return `
-        <div id="autofillautosave_moodle" class="containerOption">
+    <div id="autofillautosave_moodle" class="containerOption">
 
-            <div id="users-autofill-moodle" class="users" style="display: none;">
+    <div id="users-autofill-moodle" class="users" style="display: none;">
 
-                <!-- Columna para mostrar el nombre de usuario actual con icono de usuario -->
-                <div class="usuario-actual">
-                    <i class="fa-solid fa-user"></i>
-                    <span id="nombre-usuario-actual" class="nombre-usuario"></span>
-                </div>
+        <!-- Columna para mostrar el nombre de usuario actual con icono de usuario -->
+        <div class="usuario-actual">
+            <i class="fa-solid fa-user"></i>
+            <span id="nombre-usuario-actual" class="nombre-usuario"></span>
+        </div>
 
-                <select id="siguiente-usuario" class="select-siguiente-usuario">
-                </select>
+        <select id="siguiente-usuario" class="select-siguiente-usuario">
+        </select>
 
-                <button id="boton-siguiente-usuario" class="boton-siguiente-usuario">
-                    <i class="fa-solid fa-angles-right"></i>
-                </button>
-
-            </div>
-
-            <!-- Contenedor para Ruta y Ciclo -->
-            <div id="containerRutaFirebase" style="display: none;">
-            </div>
-
-            <!-- Contenedor para Subject Dinamic -->
-            <div id="containerRutaDinamicaFirebase" style="display: none;">
-            </div>
-
-            <div id="container-autofillautosave">
-
-            
-
-            <!-- Nuevo contenedor para AutoFill-->
-            <div id="container-autofill" class="subcontainer-autoquiz-autofill">
-
-                <div id="header-autofill" class="header-autoquiz">
-                    <h2 id="titulo-autoquiz">AutoFill</h2>
-
-                    <label class="switch-autoquiz">
-                        <input type="checkbox" id="switch-autofill">
-                        <span class="slider round"></span>
-                    </label>
-                </div>
-
-                <!-- Contenido de AutoFill-->
-                <div id="body-autoquiz-autofill" class="body-autoquiz" style="display: none;">
-
-                    <!-- Contenedor dinámico para las preguntas generadas -->
-                    <div id="contenedor-preguntas" class="contenedor-preguntas">
-                        <!-- Aquí se insertarán dinámicamente los detalles de cada pregunta -->
-                    </div>
-
-                </div>
-            </div>
-
-
-
-
-<!-- Contenedor para AutoSave -->
-<div id="container-autosave" class="subcontainer-autoquiz-autosave">
-
-    <div id="header-autosave" class="header-autoquiz">
-
-        <!-- Título y Botón -->
-        <h2 id="titulo-autoquiz">
-            AutoSave
-
-            <button id="upload-autosave" class="icon-button">
-                <i class="fa-solid fa-cloud-arrow-up"></i>
-            </button>
-
-        </h2>
-
-        <label class="switch-autoquiz">
-            <input type="checkbox" id="switch-autosave">
-            <span class="slider round"></span>
-        </label>
+        <button id="boton-siguiente-usuario" class="boton-siguiente-usuario">
+            <i class="fa-solid fa-angles-right"></i>
+        </button>
 
     </div>
 
-    <!-- Contenido de AutoSave -->
-    <div id="body-autoquiz-autosave">
-        <div>
-            <span id="respuestasautosave"></span>
-        </div>
+    <!-- Contenedor para Ruta y Ciclo -->
+    <div id="containerRutaFirebase" style="display: none;">
     </div>
-</div>
 
-</div>
+    <!-- Contenedor para Subject Dinamic -->
+    <div id="containerRutaDinamicaFirebase" style="display: none;">
+    </div>
+
+    <div id="container-autofillautosave">
+
+
+
+        <!-- Nuevo contenedor para AutoFill-->
+        <div id="container-autofill" class="subcontainer-autoquiz-autofill">
+
+            <div id="header-autofill" class="header-autoquiz">
+                <h2 id="titulo-autoquiz">AutoFill</h2>
+
+                <label class="switch-autoquiz">
+                    <input type="checkbox" id="switch-autofill">
+                    <span class="slider round"></span>
+                </label>
+            </div>
+
+            <!-- Contenido de AutoFill-->
+            <div id="body-autoquiz-autofill" class="body-autoquiz" style="display: none;">
+
+                <!-- Contenedor dinámico para las preguntas generadas -->
+                <div id="contenedor-preguntas" class="contenedor-preguntas">
+                    <!-- Aquí se insertarán dinámicamente los detalles de cada pregunta -->
+                </div>
+
+            </div>
         </div>
+
+
+
+
+        <!-- Contenedor para AutoSave -->
+        <div id="container-autosave" class="subcontainer-autoquiz-autosave">
+
+            <div id="header-autosave" class="header-autoquiz">
+
+                <!-- Título y Botón -->
+                <h2 id="titulo-autoquiz">
+                    AutoSave
+
+                    <button id="upload-autosave" class="icon-button">
+                        <i class="fa-solid fa-cloud-arrow-up"></i>
+                    </button>
+
+                </h2>
+
+                <label class="switch-autoquiz">
+                    <input type="checkbox" id="switch-autosave">
+                    <span class="slider round"></span>
+                </label>
+
+            </div>
+
+            <!-- Contenido de AutoSave -->
+            <div id="body-autoquiz-autosave">
+                <div>
+                    <span id="respuestasautosave"></span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>   
     `;
 	}
 
